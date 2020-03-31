d0a6bf76155a51d73aec0ddbac439e186aafdabd
peetphatcharee <33540551+peetphatcharee@users.noreply.github.com>
2020-04-01 01:52:27 +0700

    ีup2

--- app/src/main/java/th/ac/kku/cis/mobileapp/phamacyfinal/Adapter.kt
+++ app/src/main/java/th/ac/kku/cis/mobileapp/phamacyfinal/Adapter.kt
@@ -55,7 +55,60 @@ class CategoryAdapter(context: android.content.Context, toDoItemList: MutableLis
 }
 
 
+class PhamacyAdapter (context: android.content.Context, toDoPhamacyList: MutableList<Phamacy>) : BaseAdapter() {
 
+    private val mInflater: LayoutInflater = LayoutInflater.from(context)
+    private var itemList = toDoPhamacyList
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        // create object from view
+    //    val Newname: String = itemList.get(position).NewName as String
+        val itemPha: String = itemList.get(position).itemPha as String
+        val cure: String = itemList.get(position).cure as String
+        val more: String = itemList.get(position).more as String
+
+        val view: View
+        val vh: ListRowHolder
+
+        // get list view
+        if (convertView == null) {
+            view = mInflater.inflate(R.layout.activity_phamacy_item, parent, false)
+            vh = ListRowHolder(view)
+            view.tag = vh
+        } else {
+            view = convertView
+            vh = view.tag as ListRowHolder
+        }
+
+        // add text to view
+        vh.label2.text = itemPha
+        vh.label3.text = cure
+        vh.label4.text = more
+       // vh.label5.text = Newname
+
+        return view
+    }
+
+    override fun getItem(index: Int): Any {
+        return itemList.get(index)
+    }
+
+    override fun getItemId(index: Int): Long {
+        return index.toLong()
+    }
+
+    override fun getCount(): Int {
+        return itemList.size
+    }
+
+    private class ListRowHolder(row: View?) {
+        val label2: TextView = row!!.findViewById<TextView>(R.id.textView2) as TextView
+        val label3: TextView = row!!.findViewById<TextView>(R.id.textView4) as TextView
+        val label4: TextView = row!!.findViewById<TextView>(R.id.textView8) as TextView
+    //    val label5: TextView = row!!.findViewById<TextView>(R.id.textView12) as TextView
+    }
+}
 
 
 
--- app/src/main/java/th/ac/kku/cis/mobileapp/phamacyfinal/listPhamacy.kt
+++ app/src/main/java/th/ac/kku/cis/mobileapp/phamacyfinal/listPhamacy.kt
@@ -1,16 +1,19 @@
 package th.ac.kku.cis.mobileapp.phamacyfinal
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_list_phamacy.*
 
 class listPhamacy : AppCompatActivity() {
-
+    lateinit var mDatabase: DatabaseReference
+    lateinit var adapter: PhamacyAdapter
+    private var listViewItems: ListView? = null
+    var toDoPhamacyList: MutableList<Phamacy>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +31,44 @@ class listPhamacy : AppCompatActivity() {
             i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             i.putExtra("name1", name)
             startActivity(i)
-
         }
+//
+
+
+        listViewItems = findViewById<View>(R.id.view2) as ListView
+        toDoPhamacyList = mutableListOf<Phamacy>()
+        adapter = PhamacyAdapter(this, toDoPhamacyList!!)
+        listViewItems!!.setAdapter(adapter)
+
+        mDatabase = FirebaseDatabase.getInstance().reference
+        mDatabase.child("Phamacy").addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val items = dataSnapshot.children.iterator()
+
+                var name = getIntent().getStringExtra("name1") //รับมา
+
+                // Check if current database contains any collection
+                if (items.hasNext()) {
+                    while (items.hasNext()) {
+                        val toDoListindex = items.next()
+                        val map = toDoListindex.getValue() as HashMap<String, Any>
+
+                        if (map.get("newName") == name) {
+                            // add data to object
+                            val todoItem = Phamacy.create()
+                          //  todoItem.NewName = map.get("newName") as String?
+                            todoItem.itemPha = map.get("itemPha") as String?
+                            todoItem.cure = map.get("cure") as String?
+                            todoItem.more = map.get("more") as String?
+                            toDoPhamacyList!!.add(todoItem);
+                            adapter.notifyDataSetChanged()
+                        }
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        })
 
     }
 }
